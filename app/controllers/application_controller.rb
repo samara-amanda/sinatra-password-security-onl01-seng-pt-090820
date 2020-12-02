7553db185c2e2af28f5d7c5358156501e953fb69
Samara Grant <samara.amanda01@gmail.com>
2020-12-02 01:27:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,6 +33,7 @@ class ApplicationController < Sinatra::Base
 	post "/login" do
 		#your code here!
 		user = User.find_by(:username => params[:username])
+
 		if user && user.authenticate(params[:username])
 			session[:user_id] = user.id
 			redirect '/success'
